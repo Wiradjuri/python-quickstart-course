31b9c3e4acbfb97855039d3e9f482a3022bffa86
wiradjuri <bmuzza1992@gmail.com>
2025-08-08 18:32:30 +1000

    Fix GitHub Actions: Update Python version requirements to 3.8+ (remove deprecated 3.7)

--- setup.py
+++ setup.py
@@ -37,8 +37,6 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -46,7 +44,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=read_requirements(),
     include_package_data=True,
     package_data={
